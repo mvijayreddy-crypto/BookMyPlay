853cb15a199b67476766c3e45454cfba0a942bd3
vijayreddy <vijayreddy@vijayreddys-MacBook-Air.local>
2026-05-06 02:07:37 +0530

    first commit

--- app.py
+++ app.py
@@ -258,6 +258,9 @@ def logout():
     return redirect('/login')
 
 # ================= RUN =================
+from flask import Flask
 import os
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+app = Flask(__name__)
+
+os.makedirs("instance", exist_ok=True)
