4bae881a9773c13f1a4043e70f5d4e55ef5d5c90
vijayreddy <vijayreddy@vijayreddys-MacBook-Air.local>
2026-05-06 01:47:09 +0530

    first commit

--- app.py
+++ app.py
@@ -258,5 +258,6 @@ def logout():
     return redirect('/login')
 
 # ================= RUN =================
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
